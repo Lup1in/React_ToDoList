bb72d5379306fe2965b0c016f4621b15c951da58
Lup1in <gabrielcunhaleite@hotmail.com>
2023-11-17 16:46:45 -0300

    react-icons, main, main.css, editorconfig & eslintrc

---
+++ src/components/main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+import './main.css';
+
+export default class main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <form action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
